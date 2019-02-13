2c6726da5ec5a8a9c86a232ae4da56cfe799b33b
Adrian <prietolozano.adrian@gmail.com>
2019-02-12 23:20:15 -0600

    se agregaron las casas en renta

--- informador2.py
+++ informador2.py
@@ -11,27 +11,35 @@ class Informador:
         with open(datetime.datetime.now().strftime("%Y-%m-%d") + ".json", "w") as archivo:
              json.dump(self.lista, archivo, sort_keys=False, indent=4)
 
+
     def scrapping(self):
-        url = "http://aviso.informador.com.mx/index.php/bienes_raices/busqueda?selecciono=0&ciudad_autocomplete=0&colonia_autocomplete=&transaccion=1&tipo=1&consulta=Zona+metropolitana&precio_min=min&precio_max=max&recamaras_min=0&recamaras_max=0&metros_min=0&metros_max=0&quick-search=Zona+metropolitana-&quick-searchZap=Zapopan-3&quick-searchGdl=Guadalajara-2&quick-searchTlaq=Tlaquepaque-5&quick-searchTon=Tonal%C3%A1-4"
-        r = requests.get(url)
-        r.encoding = "utf-8"
+        url_venta = "http://aviso.informador.com.mx/index.php/bienes_raices/busqueda?selecciono=0&ciudad_autocomplete=0&colonia_autocomplete=&transaccion=1&tipo=1&consulta=Zona+metropolitana&precio_min=min&precio_max=max&recamaras_min=0&recamaras_max=0&metros_min=0&metros_max=0&quick-search=Zona+metropolitana-&quick-searchZap=Zapopan-3&quick-searchGdl=Guadalajara-2&quick-searchTlaq=Tlaquepaque-5&quick-searchTon=Tonal%C3%A1-4"
+        url_renta = "http://aviso.informador.com.mx/index.php/bienes_raices/busqueda?selecciono=1&ciudad_autocomplete=&colonia_autocomplete=&transaccion=2&tipo=1&consulta=Zona+metropolitana&precio_min=min&precio_max=max&recamaras_min=0&recamaras_max=0&metros_min=0&metros_max=0&quick-search=Zona+metropolitana-&quick-searchZap=Zapopan-3&quick-searchGdl=Guadalajara-2&quick-searchTlaq=Tlaquepaque-5&quick-searchTon=Tonal%C3%A1-4"
 
-        soup = BeautifulSoup(r.text, "html.parser")
-        items = soup.find_all(class_="items")
-        casas = items[0].find_all("li")
-
-        self.scrapping_casas(casas)
-
-        paginas = soup.find(class_="pagination")
-        paginas = paginas.find_all("li")
-
+        urls_principales = [url_venta, url_renta]
         urls = []
-        for p in paginas[2:len(paginas)-1]:
-            urls.append(p.a["href"])
+
+        for url in urls_principales:
+            r = requests.get(url)
+            r.encoding = "utf-8"
+
+            soup = BeautifulSoup(r.text, "html.parser")
+            items = soup.find_all(class_="items")
+            casas = items[0].find_all("li")
+            tipo = soup.find_all(selected="selected")[0].text.replace(" ", "")
+            self.scrapping_casas(casas, tipo)
+
+            paginas = soup.find(class_="pagination")
+            paginas = paginas.find_all("li")
+
+            for p in paginas[2:len(paginas) - 1]:
+                urls.append(p.a["href"])
+                print(p.a["href"])
 
         self.scrapping_paginas(urls)
 
-    def scrapping_casas(self, casas):
+
+    def scrapping_casas(self, casas, tipo):
         for c in casas:
             casa = {
                 "ubicacion": c.find_all(class_="location")[0].text,
@@ -45,19 +53,19 @@ class Informador:
                 "cars": c.find(class_="info-cars").text,
                 "colonia": c.find(class_="info-gps").contents[1],
                 "imgs": ["http:" + i["src"] for i in c.find_all("img")],
+                "tipo": tipo
             }
             self.lista.append(casa)
+
 
     def scrapping_paginas(self, urls):
         for url in urls:
             r = requests.get(url)
             r.encoding = "utf-8"
-            # print(r.text)
 
             soup = BeautifulSoup(r.text, "html.parser")
-            # print(soup)
             items = soup.find_all(class_="items")
-            # print(items)
             casas = items[0].find_all("li")
+            tipo = soup.find_all(selected="selected")[0].text.replace(" ", "")
 
-            self.scrapping_casas(casas)
+            self.scrapping_casas(casas, tipo)
